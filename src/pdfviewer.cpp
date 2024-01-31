5c293f6cf4361b2065d0b070c6364d77fefe6222
Jussi Pakkanen <jpakkane@gmail.com>
2024-01-31 19:03:42 +0200

    Update GTK flags to remove deprecation.

--- src/pdfviewer.cpp
+++ src/pdfviewer.cpp
@@ -536,7 +536,7 @@ void activate(GtkApplication *, gpointer user_data) {
 int main(int argc, char **argv) {
     App app;
     app.ifile = "title.pdf";
-    app.app = gtk_application_new("io.github.jpakkane.pdfviewer", G_APPLICATION_FLAGS_NONE);
+    app.app = gtk_application_new("io.github.jpakkane.pdfviewer", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app.app, "activate", G_CALLBACK(activate), static_cast<gpointer>(&app));
     int status = g_application_run(G_APPLICATION(app.app), argc, argv);
     g_object_unref(app.app);
