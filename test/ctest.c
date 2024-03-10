47c5039dd083599e1fa70d4a8b4e627347652d29
Jussi Pakkanen <jpakkane@gmail.com>
2024-03-10 19:08:29 +0200

    Update C test to actually create an output file.

--- test/ctest.c
+++ test/ctest.c
@@ -3,18 +3,28 @@
 
 #include <capypdf.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
     CapyPDF_EC rc;
     CapyPDF_Generator *gen;
     CapyPDF_Options *opt;
+    CapyPDF_DrawContext *dc;
+    const char *fname = "capy_ctest.pdf";
+    FILE *f;
+    unlink(fname);
+    f = fopen(fname, "r");
+    if(f) {
+        fprintf(stderr, "Test file exists.\n");
+        return 1;
+    }
 
     if((rc = capy_options_new(&opt)) != 0) {
         fprintf(stderr, "%s\n", capy_error_message(rc));
         return 1;
     }
 
-    if((rc = capy_generator_new("dummy.pdf", opt, &gen)) != 0) {
+    if((rc = capy_generator_new(fname, opt, &gen)) != 0) {
         fprintf(stderr, "%s\n", capy_error_message(rc));
         return 1;
     }
@@ -24,9 +34,52 @@ int main() {
         return 1;
     }
 
+    if((rc = capy_page_draw_context_new(gen, &dc)) != 0) {
+        fprintf(stderr, "%s\n", capy_error_message(rc));
+        return 1;
+    }
+
+    if((rc = capy_dc_cmd_rg(dc, 1.0, 0.1, 0.5)) != 0) {
+        fprintf(stderr, "%s\n", capy_error_message(rc));
+        return 1;
+    }
+
+    if((rc = capy_dc_cmd_re(dc, 100, 100, 200, 200)) != 0) {
+        fprintf(stderr, "%s\n", capy_error_message(rc));
+        return 1;
+    }
+
+    if((rc = capy_dc_cmd_f(dc)) != 0) {
+        fprintf(stderr, "%s\n", capy_error_message(rc));
+        return 1;
+    }
+
+    if((rc = capy_generator_add_page(gen, dc)) != 0) {
+        fprintf(stderr, "%s\n", capy_error_message(rc));
+        return 1;
+    }
+
+    if((rc = capy_dc_destroy(dc)) != 0) {
+        fprintf(stderr, "%s\n", capy_error_message(rc));
+        return 1;
+    }
+
+    if((rc = capy_generator_write(gen)) != 0) {
+        fprintf(stderr, "%s\n", capy_error_message(rc));
+        return 1;
+    }
+
     if((rc = capy_generator_destroy(gen)) != 0) {
         fprintf(stderr, "%s\n", capy_error_message(rc));
         return 1;
     }
+
+    f = fopen(fname, "r");
+    if(!f) {
+        fprintf(stderr, "Output file not created.\n");
+        return 1;
+    }
+    fclose(f);
+    unlink(fname);
     return 0;
 }
