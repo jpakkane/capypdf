e816c026e1a1e2cc71052cc7ba5431433f3d5f4d
Jussi Pakkanen <jpakkane@gmail.com>
2024-11-15 17:10:14 +0200

    Refactor context.

--- tools/printertest.py
+++ tools/printertest.py
@@ -4,8 +4,9 @@
 # Copyright 2023-2024 Jussi Pakkanen
 
 import pathlib, os, sys, json
-
-os.environ['CAPYPDF_SO_OVERRIDE'] = 'build/src'
+ename = 'CAPYPDF_SO_OVERRIDE'
+if ename not in os.environ:
+    os.environ[ename] = 'build/src'
 source_root = pathlib.Path(__file__).parent / '..'
 sys.path.append(str(source_root / 'python'))
 
@@ -36,27 +37,29 @@ class PrinterTest:
         self.pdfgen = capypdf.Generator(self.ofilename, opts)
         self.basefont = self.pdfgen.load_font(self.fontfile)
 
-    def render_centered(self, ctx, text, font, pointsize, x, y):
+    def render_centered(self, text, font, pointsize, x, y):
         text_w = self.pdfgen.text_width(text, font, pointsize)
-        ctx.render_text(text, font, pointsize, x -text_w/2, y)
+        self.ctx.render_text(text, font, pointsize, x -text_w/2, y)
 
     def create(self):
-        with self.pdfgen.page_draw_context() as ctx:
-            self.render_centered(ctx, "CapyPDF printer test page", self.basefont, 20, self.w/2, 800)
-            with ctx.push_gstate():
-                ctx.translate(10, 700)
-                self.draw_grays(ctx)
-            with ctx.push_gstate():
-                ctx.translate(10, 600)
-                self.draw_richblacks(ctx)
-            with ctx.push_gstate():
-                ctx.translate(50, 500)
-                self.draw_op(ctx)
-            with ctx.push_gstate():
-                ctx.translate(50, 250)
-                self.draw_lab(ctx)
+        with self.pdfgen.page_draw_context() as page1:
+            self.ctx = page1
+            self.render_centered("CapyPDF printer test page", self.basefont, 20, self.w/2, 800)
+            with self.ctx.push_gstate():
+                self.ctx.translate(10, 700)
+                self.draw_grays()
+            with self.ctx.push_gstate():
+                self.ctx.translate(10, 600)
+                self.draw_richblacks()
+            with self.ctx.push_gstate():
+                self.ctx.translate(50, 500)
+                self.draw_op()
+            with self.ctx.push_gstate():
+                self.ctx.translate(50, 250)
+                self.draw_lab()
+        del self.ctx
 
-    def draw_lab(self, ctx):
+    def draw_lab(self):
         import math
         c = capypdf.Color()
         labid = self.pdfgen.add_lab_colorspace(0.9505, 1.0, 1.08, -128, 127, -128, 127)
@@ -69,91 +72,91 @@ class PrinterTest:
         gridstart = 30
         for rowid in range(num_rows+1):
             L = rowid/num_rows*100
-            ctx.cmd_g(0.0)
-            ctx.render_text(f"L={int(L)}", self.basefont, 10, 0, rowid*gridh + 3)
+            self.ctx.cmd_g(0.0)
+            self.ctx.render_text(f"L={int(L)}", self.basefont, 10, 0, rowid*gridh + 3)
             for i in range(num_steps):
                 a = 128*math.cos(i/num_steps*2*3.141)
                 b = 128*math.sin(i/num_steps*2*3.141)
                 c.set_lab(labid, L, a, b)
-                ctx.set_nonstroke(c)
-                ctx.cmd_re(gridstart + i*gridw, rowid*gridh, boxw, boxh)
-                ctx.cmd_f()
+                self.ctx.set_nonstroke(c)
+                self.ctx.cmd_re(gridstart + i*gridw, rowid*gridh, boxw, boxh)
+                self.ctx.cmd_f()
 
-    def draw_op(self, ctx):
-        with ctx.push_gstate():
-            self.draw_tridot(ctx, 'No overprint')
-        with ctx.push_gstate():
+    def draw_op(self):
+        with self.ctx.push_gstate():
+            self.draw_tridot('No overprint')
+        with self.ctx.push_gstate():
             gs = capypdf.GraphicsState()
             gs.set_OP(True)
             gs.set_OPM(0)
             gsid = self.pdfgen.add_graphics_state(gs)
-            ctx.translate(150, 0)
-            ctx.cmd_gs(gsid)
-            self.draw_tridot(ctx, 'Overprint mode 0')
-        with ctx.push_gstate():
+            self.ctx.translate(150, 0)
+            self.ctx.cmd_gs(gsid)
+            self.draw_tridot('Overprint mode 0')
+        with self.ctx.push_gstate():
             gs = capypdf.GraphicsState()
             gs.set_OP(True)
             gs.set_OPM(1)
             gsid = self.pdfgen.add_graphics_state(gs)
-            ctx.translate(300, 0)
-            ctx.cmd_gs(gsid)
-            self.draw_tridot(ctx, 'Overprint mode 1')
+            self.ctx.translate(300, 0)
+            self.ctx.cmd_gs(gsid)
+            self.draw_tridot('Overprint mode 1')
 
-    def draw_tridot(self, ctx, text):
-        self.render_centered(ctx, text, self.basefont, 10, 50, 0)
-        ctx.cmd_k(1.0, 0, 0, 0)
-        self.draw_circle(ctx, 50, 50, 50+10)
-        ctx.cmd_k(0.0, 1.0, 0, 0)
-        self.draw_circle(ctx, 50, 35, 25+10)
-        ctx.cmd_k(0.0, 0.0, 1.0, 0)
-        self.draw_circle(ctx, 50, 65, 25+10)
+    def draw_tridot(self, text):
+        self.render_centered(text, self.basefont, 10, 50, 0)
+        self.ctx.cmd_k(1.0, 0, 0, 0)
+        self.draw_circle(50, 50, 50+10)
+        self.ctx.cmd_k(0.0, 1.0, 0, 0)
+        self.draw_circle(50, 35, 25+10)
+        self.ctx.cmd_k(0.0, 0.0, 1.0, 0)
+        self.draw_circle(50, 65, 25+10)
 
-    def draw_unit_circle(self, ctx):
+    def draw_unit_circle(self):
         control = 0.5523 / 2;
-        ctx.cmd_m(0, 0.5);
-        ctx.cmd_c(control, 0.5, 0.5, control, 0.5, 0);
-        ctx.cmd_c(0.5, -control, control, -0.5, 0, -0.5);
-        ctx.cmd_c(-control, -0.5, -0.5, -control, -0.5, 0);
-        ctx.cmd_c(-0.5, control, -control, 0.5, 0, 0.5);
+        self.ctx.cmd_m(0, 0.5);
+        self.ctx.cmd_c(control, 0.5, 0.5, control, 0.5, 0);
+        self.ctx.cmd_c(0.5, -control, control, -0.5, 0, -0.5);
+        self.ctx.cmd_c(-control, -0.5, -0.5, -control, -0.5, 0);
+        self.ctx.cmd_c(-0.5, control, -control, 0.5, 0, 0.5);
 
-    def draw_and_fill_unit_circle(self, ctx):
-        self.draw_unit_circle(ctx)
-        ctx.cmd_f()
+    def draw_and_fill_unit_circle(self):
+        self.draw_unit_circle()
+        self.ctx.cmd_f()
 
-    def draw_circle(self, ctx, scale, offx, offy):
-        with ctx.push_gstate():
-            ctx.translate(offx, offy)
-            ctx.scale(scale, scale)
-            self.draw_and_fill_unit_circle(ctx)
+    def draw_circle(self, scale, offx, offy):
+        with self.ctx.push_gstate():
+            self.ctx.translate(offx, offy)
+            self.ctx.scale(scale, scale)
+            self.draw_and_fill_unit_circle()
 
-    def draw_grays(self, ctx):
-        ctx.cmd_g(0.5)
+    def draw_grays(self):
+        self.ctx.cmd_g(0.5)
         deltax = 100
-        self.draw_gray_and_text(ctx, 50, 80, 50, "G gray")
-        ctx.cmd_k(0.5, 0.5, 0.5, 0)
-        self.draw_gray_and_text(ctx, 50, 80 + deltax, 50, "CMY gray")
-        ctx.cmd_k(0.5, 0.5, 0.5, 0.5)
-        self.draw_gray_and_text(ctx, 50, 80 + 2*deltax, 50, "CMYK gray")
-        ctx.cmd_k(0.0, 0.0, 0.0, 0.5)
-        self.draw_gray_and_text(ctx, 50, 80 + 3*deltax, 50, "K gray")
-        ctx.cmd_rg(0.5, 0.5, 0.5)
-        self.draw_gray_and_text(ctx, 50, 80 + 4*deltax, 50, "RGB gray")
+        self.draw_gray_and_text(50, 80, 50, "G gray")
+        self.ctx.cmd_k(0.5, 0.5, 0.5, 0)
+        self.draw_gray_and_text(50, 80 + deltax, 50, "CMY gray")
+        self.ctx.cmd_k(0.5, 0.5, 0.5, 0.5)
+        self.draw_gray_and_text(50, 80 + 2*deltax, 50, "CMYK gray")
+        self.ctx.cmd_k(0.0, 0.0, 0.0, 0.5)
+        self.draw_gray_and_text(50, 80 + 3*deltax, 50, "K gray")
+        self.ctx.cmd_rg(0.5, 0.5, 0.5)
+        self.draw_gray_and_text(50, 80 + 4*deltax, 50, "RGB gray")
 
-    def draw_richblacks(self, ctx):
-        ctx.cmd_k(0.1, 0, 0, 1.0)
+    def draw_richblacks(self):
+        self.ctx.cmd_k(0.1, 0, 0, 1.0)
         deltax = 100
-        self.draw_gray_and_text(ctx, 50, 80, 50, "C rich black")
-        ctx.cmd_k(0.0, 0.1, 0.0, 1.0)
-        self.draw_gray_and_text(ctx, 50, 80 + deltax, 50, "M rich black")
-        ctx.cmd_k(0.0, 0.0, 0.1, 1.0)
-        self.draw_gray_and_text(ctx, 50, 80 + 2*deltax, 50, "Y rich black")
-        ctx.cmd_k(0.1, 0.1, 0.1, 1.0)
-        self.draw_gray_and_text(ctx, 50, 80 + 3*deltax, 50, "All rich black")
+        self.draw_gray_and_text(50, 80, 50, "C rich black")
+        self.ctx.cmd_k(0.0, 0.1, 0.0, 1.0)
+        self.draw_gray_and_text(50, 80 + deltax, 50, "M rich black")
+        self.ctx.cmd_k(0.0, 0.0, 0.1, 1.0)
+        self.draw_gray_and_text(50, 80 + 2*deltax, 50, "Y rich black")
+        self.ctx.cmd_k(0.1, 0.1, 0.1, 1.0)
+        self.draw_gray_and_text(50, 80 + 3*deltax, 50, "All rich black")
 
-    def draw_gray_and_text(self, ctx, s, transx, transy, text):
-        self.draw_circle(ctx, s, transx, transy)
-        ctx.cmd_g(0)
-        self.render_centered(ctx, text, self.basefont, 10, transx, transy - 40)
+    def draw_gray_and_text(self, s, transx, transy, text):
+        self.draw_circle(s, transx, transy)
+        self.ctx.cmd_g(0)
+        self.render_centered(text, self.basefont, 10, transx, transy - 40)
 
     def finish(self):
         self.pdfgen.write()
