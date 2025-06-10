8e327d83e607e076c83719ae1a75bb321de29476
Jussi Pakkanen <jussi.pakkanen@mailbox.org>
2025-06-11 00:34:08 +0300

    Migrate pdfwriter away from std::visit.

--- src/pdfwriter.cpp
+++ src/pdfwriter.cpp
@@ -248,22 +248,21 @@ rvoe<NoReturnValue> PdfWriter::write_header() {
 }
 
 rvoe<std::vector<uint64_t>> PdfWriter::write_objects() {
-    size_t i = 0;
-    auto visitor = overloaded{
-        [](DummyIndexZero &) -> rvoe<NoReturnValue> { RETOK; },
+    std::vector<uint64_t> object_offsets;
+    for(size_t i = 0; i < doc.document_objects.size(); ++i) {
+        object_offsets.push_back(ftell(ofile));
+        auto &obj = doc.document_objects.at(i);
+        if(std::holds_alternative<DummyIndexZero>(obj)) {
 
-        [&](const FullPDFObject &pobj) -> rvoe<NoReturnValue> {
-            ERCV(write_finished_object(i, pobj.dictionary, pobj.stream.span()));
-            RETOK;
-        },
-
-        [&](const DeflatePDFObject &pobj) -> rvoe<NoReturnValue> {
+        } else if(auto *pobj = std::get_if<FullPDFObject>(&obj)) {
+            ERCV(write_finished_object(i, pobj->dictionary, pobj->stream.span()));
+        } else if(auto *pobj_ = std::get_if<DeflatePDFObject>(&obj)) {
+            auto &pobj = *pobj_;
             ObjectFormatter &fmt = const_cast<ObjectFormatter &>(pobj.unclosed_dictionary);
             if(pobj.leave_uncompressed_in_debug && !doc.docprops.compress_streams) {
                 fmt.add_token_pair("/Length", pobj.stream.size());
                 fmt.end_dict();
                 ERCV(write_finished_object(i, fmt.steal(), pobj.stream.span()));
-                RETOK;
             } else {
                 ERC(compressed, flate_compress(pobj.stream.sv()));
                 // FIXME, not great.
@@ -271,69 +270,35 @@ rvoe<std::vector<uint64_t>> PdfWriter::write_objects() {
                 fmt.add_token_pair("/Length", compressed.size());
                 fmt.end_dict();
                 ERCV(write_finished_object(i, fmt.steal(), compressed));
-                RETOK;
             }
-        },
-
-        [&](const DelayedSubsetFontData &ssfont) -> rvoe<NoReturnValue> {
-            ERCV(write_subset_font_data(i, ssfont));
-            RETOK;
-        },
-
-        [&](const DelayedSubsetFontDescriptor &ssfontd) -> rvoe<NoReturnValue> {
+        } else if(auto *ssfont = std::get_if<DelayedSubsetFontData>(&obj)) {
+            ERCV(write_subset_font_data(i, *ssfont));
+        } else if(auto *ssfontd = std::get_if<DelayedSubsetFontDescriptor>(&obj)) {
             ERCV(write_subset_font_descriptor(i,
-                                              doc.fonts.at(ssfontd.fid.id).fontdata,
-                                              ssfontd.subfont_data_obj,
-                                              ssfontd.subset_num));
-            RETOK;
-        },
-
-        [&](const DelayedSubsetCMap &sscmap) -> rvoe<NoReturnValue> {
-            assert(sscmap.subset_id == 0);
-            ERCV(write_subset_cmap(i, doc.fonts.at(sscmap.fid.id)));
-            RETOK;
-        },
-
-        [&](const DelayedSubsetFont &ssfont) -> rvoe<NoReturnValue> {
-            ERCV(write_subset_font(i, doc.fonts.at(ssfont.fid.id), ssfont.subfont_cmap_obj));
-            RETOK;
-        },
-
-        [&](const DelayedCIDDictionary &ciddict) -> rvoe<NoReturnValue> {
-            ERCV(write_cid_dict(i, ciddict.fid, ciddict.subfont_descriptor_obj));
-            RETOK;
-        },
-
-        [&](const DelayedPages &) -> rvoe<NoReturnValue> {
+                                              doc.fonts.at(ssfontd->fid.id).fontdata,
+                                              ssfontd->subfont_data_obj,
+                                              ssfontd->subset_num));
+        } else if(auto *sscmap = std::get_if<DelayedSubsetCMap>(&obj)) {
+            assert(sscmap->subset_id == 0);
+            ERCV(write_subset_cmap(i, doc.fonts.at(sscmap->fid.id)));
+        } else if(auto *ssfont = std::get_if<DelayedSubsetFont>(&obj)) {
+            ERCV(write_subset_font(i, doc.fonts.at(ssfont->fid.id), ssfont->subfont_cmap_obj));
+        } else if(auto *ciddict = std::get_if<DelayedCIDDictionary>(&obj)) {
+            ERCV(write_cid_dict(i, ciddict->fid, ciddict->subfont_descriptor_obj));
+        } else if(std::holds_alternative<DelayedPages>(obj)) {
             ERCV(write_pages_root());
-            RETOK;
-        },
-
-        [&](const DelayedPage &dp) -> rvoe<NoReturnValue> {
-            ERCV(write_delayed_page(dp));
-            RETOK;
-        },
-
-        [&](const DelayedCheckboxWidgetAnnotation &checkbox) -> rvoe<NoReturnValue> {
-            ERCV(write_checkbox_widget(i, checkbox));
-            RETOK;
-        },
-
-        [&](const DelayedAnnotation &anno) -> rvoe<NoReturnValue> {
-            ERCV(write_annotation(i, anno));
-            RETOK;
-        },
-
-        [&](const DelayedStructItem &si) -> rvoe<NoReturnValue> {
-            ERCV(write_delayed_structure_item(i, si));
-            RETOK;
-        },
-    };
-
-    std::vector<uint64_t> object_offsets;
-    for(; i < doc.document_objects.size(); ++i) {
-        object_offsets.push_back(ftell(ofile));
-        ERCV(std::visit(visitor, doc.document_objects.at(i)));
+        } else if(auto *dp = std::get_if<DelayedPage>(&obj)) {
+            ERCV(write_delayed_page(*dp));
+        } else if(auto *checkbox = std::get_if<DelayedCheckboxWidgetAnnotation>(&obj)) {
+            ERCV(write_checkbox_widget(i, *checkbox));
+        } else if(auto *anno = std::get_if<DelayedAnnotation>(&obj)) {
+            ERCV(write_annotation(i, *anno));
+        } else if(auto *si = std::get_if<DelayedStructItem>(&obj)) {
+            ERCV(write_delayed_structure_item(i, *si));
+        } else {
+            fprintf(stderr, "Unreachable variant visit.\n");
+            std::abort();
+        }
     }
     return object_offsets;
 }
